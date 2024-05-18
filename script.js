1c94b5bfd66ddd66b0c0c6b8d95a7491d0f2c073
jonatan <jonatanpedalino133@gmail.com>
2024-05-17 23:04:40 -0300

    terminado

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("h3captured")
 
 var sueldo = Number(prompt("ingresa el sueldo sin puntos barbarita, jajaja "))
-var porsentaje = Number(prompt("ingresa el porsentaje"))
+var porsentaje = Number(prompt("ingresa el porsentaje mi amor!!!"))
 
 function porsentajeSueldo (a,b) {
     var b = (sueldo * porsentaje) / 100 + sueldo
@@ -10,3 +10,5 @@ function porsentajeSueldo (a,b) {
 }
 
 alert("barbarita vas a cobrar " + porsentajeSueldo())
+alert("podrias hacerme un prestamo jajaja")
+
